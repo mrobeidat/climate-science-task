735c10731907ef4fccae6596448f20e3b4870f98
Yousef Obeidat <y.linux96@gmail.com>
2022-03-04 02:48:31 +0300

    fix-positions

--- components/Frame13.tsx
+++ components/Frame13.tsx
@@ -1,29 +1,54 @@
+import { useEffect, useState } from "react";
+
 export default function Frame13() {
-    return (
-        <>
-            <div id="frame13" className="container">
-                <p>Which of the below statements<br/> about electricity is not true?</p>
-                <div className="choices">
-                    <label className="rad-label">
-                        <input type="radio" className="rad-input" name="rad" />
-                        <div className="rad-design"></div>
-                        <div className="rad-text">Electricity is measured in units called watts</div>
-                    </label>
+    const [checked, setChecked] = useState(false);
+    useEffect(() => {
+			setInterval(() => {
+				setChecked(true);
+			}, 2500);
+			setInterval(() => {
+				setChecked(false);
+			}, 1500);
+		}, [setChecked]);
 
-                    <label className="rad-label">
-                        <input type="radio" className="rad-input" name="rad" />
-                        <div className="rad-design"></div>
-                        <div className="rad-text">Electricity flows at the speed of light</div>
-                    </label>
+	return (
+		<>
+			<div id="frame13" className="container">
+				<p>
+					Which of the below statements
+					<br /> about electricity is not true?
+				</p>
+				<div className="choices">
+					<label className="rad-label">
+						<input type="radio" className="rad-input" name="rad" />
+						<div className="rad-design"></div>
+						<div className="rad-text">
+							Electricity is measured in units called watts
+						</div>
+					</label>
 
-                    <label className="rad-label">
-                        <input type="radio" className="rad-input" name="rad" />
-                        <div className="rad-design"></div>
-                        <div className="rad-text">Electricity is a primary energy source</div>
-                    </label>
-                </div>
+					<label className="rad-label">
+						<input type="radio" className="rad-input" name="rad" />
+						<div className="rad-design"></div>
+						<div className="rad-text">
+							Electricity flows at the speed of light
+						</div>
+					</label>
 
-            </div>
-        </>
-    )
+					<label className="rad-label">
+						<input
+							type="radio"
+							className="rad-input"
+							name="thechecked"
+							{...checked && { checked: true }}
+						/>
+						<div className="rad-design"></div>
+						<div className="rad-text">
+							Electricity is a primary energy source
+						</div>
+					</label>
+				</div>
+			</div>
+		</>
+	);
 }
