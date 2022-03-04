727fb05014cc60ce0a784c9a5b78682067638adb
Yousef Obeidat <y.linux96@gmail.com>
2022-03-04 03:23:38 +0300

    edit-logo-position

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 
 export default function Header() {
     return (
-        <nav>
+        <nav >
             <Link href="#">ClimateScience</Link>
             <Link href="/">Home</Link>
             <Image width="132px" height="72px" src='/logo.svg' alt="" />
